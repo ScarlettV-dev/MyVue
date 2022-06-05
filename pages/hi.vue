de68a944776f93bf66813cefaf4c599eed371e2e
Arlett Vega <arlettvega0221@gmail.com>
2022-06-05 13:05:59 -0400

    Creating hi.vue

--- pages/hi.vue
+++ pages/hi.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1>Suffering with this (tragic sounds*)</h1>
+  <div>
+    <div class="header-title">
+      <h1>Suffering with Vue (tragic sounds*)</h1>
+      <h2>Another tragic sound</h2>
+    </div>
+    <br>
+
+    <div>
+      <p>You wrote: {{ message }}</p>
+      <input class="add-content" v-model="message" placeholder="Write something" />
+    </div>
+    <a @[eventName]="doSomething">Click here</a>
+  </div>
+
 </template>
+
+<script>
+export default {
+  name: 'HiPage'
+}
+</script>
+
+<style>
+  .header-title{
+    color:peru;
+    font-family: 'Courier New', Courier, monospace;
+  }
+
+  .add-content{
+    border-radius: 15px;
+  }
+
+  /* .color{
+    color:black;
+  } */
+
+  .v-main{
+    background: #dcd3e9;
+  }
+</style>
