fd5e78cdd4876fd617f8fce4c17b41565275c51a
Arlett Vega <arlettvega0221@gmail.com>
2022-06-05 14:35:19 -0400

    Create index.vue

---
+++ pages/games/_id/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="single-game">
+    <h1>{{ $route.params.id }}</h1>
+    <div>
+      <img src="">
+    </div>
+    <p>Deeper game description</p>
+  </section>
+  <!-- <p>{{ $route.params.id }}</p> -->
+</template>
+
+<style scoped>
+  .single-game{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: column;
+    text-align: center;
+    padding: 30px;
+  }
+</style>
