95c019a1791870f68237acce51cd75e889c3020e
LarryGF <larrywtf609@gmail.com>
2022-06-05 20:44:26 +0100

    Added picture from static

--- pages/games/index.vue
+++ pages/games/index.vue
@@ -7,7 +7,7 @@
      description="First Person Shooter"/>
 
      <Game
-     pic="https://www.google.com/imgres?imgurl=https%3A%2F%2Fs3.amazonaws.com%2Fprod-media.gameinformer.com%2Fstyles%2Fthumbnail%2Fs3%2Flegacy-images%2FBeauty%2520In%2520Death%2Foriandblindforesthero.jpg&imgrefurl=https%3A%2F%2Fwww.gameinformer.com%2Fgames%2Fori_and_the_blind_forest%2Fb%2Fxboxone%2Farchive%2F2015%2F03%2F09%2Fbeauty-in-death.aspx&tbnid=YK8RzEY49ynBcM&vet=12ahUKEwiW9ZHU65b4AhWRs3IEHQODC2EQMygNegUIARDXAQ..i&docid=3dOvlWhQaU9yWM&w=800&h=450&q=ori%20and%20the%20blind%20forest&hl=es-419&ved=2ahUKEwiW9ZHU65b4AhWRs3IEHQODC2EQMygNegUIARDXAQ"
+     pic="ori.jpeg"
      title="Ori and the Blind Forest"
      id="2"
      description="Adventure Game"/>
