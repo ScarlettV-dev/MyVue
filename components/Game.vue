04a271eda53b2da50e7703fe22717db013062679
Arlett Vega <arlettvega0221@gmail.com>
2022-06-05 14:35:26 -0400

    Create Game.vue

---
+++ components/Game.vue
@@ -0,0 +1,37 @@
+<template>
+  <NuxtLink :to="'/games/' + id">
+    <article class="game">
+      <div class="pic" :style="{backgroundImage:'url(' + pic + ')'}"></div>
+        <h1>{{title}}</h1>
+        <p>{{description}}</p>
+    </article>
+  </NuxtLink>
+</template>
+
+<script>
+  export default{
+    props: ["pic", "title", "description", "id"]
+  }
+</script>
+
+<style scoped>
+  a{
+    text-decoration:none;
+    color: black;
+  }
+  .game{
+      box-sizing: border-box;
+      width: 280px;
+      padding:8px;
+      border: 1px solid #ccc;
+      box-shadow: 0 2px 2px #aaa;
+      margin: 10px;
+    }
+
+    .pic{
+      background-position: center;
+      background-size: cover;
+      width: 100%;
+      height: 200px;
+    }
+</style>
